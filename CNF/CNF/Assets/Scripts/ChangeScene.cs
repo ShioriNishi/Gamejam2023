9ca34df3889ed16a0e63d343630033e17e163512
Chiharu Tanaka <c_tanaka@acquire.co.jp>
2023-09-13 09:01:21 +0900

    add&update:PrologueSceneにシーン移動用のボタンを追加しました。また、Charaの表示順を調整しました。

---
+++ CNF/CNF/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] private Image _PanelImage;
+    [SerializeField] private float _speed;
+    private bool isSceneChange;
+    private Color PanelColor;
+    private void Awake()
+    {
+        isSceneChange = false;
+        PanelColor = _PanelImage.color;
+    }
+    public void blackout()
+    {
+        StartCoroutine(Sceneblackout());
+    }
+    private IEnumerator Sceneblackout()
+    {
+        while (!isSceneChange)
+        {
+            PanelColor.a += 0.01f;
+            _PanelImage.color = PanelColor;
+            if (PanelColor.a >= 1)
+                isSceneChange = true;
+            yield return new WaitForSeconds(_speed);
+        }
+        SceneManager.LoadScene("Main");
+    }
+}
