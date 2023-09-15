c8fbc1d53c8d29ae2a791e6c0c7fbbdd39673a27
acq_s_nishi <s_nishi@acquire.co.jp>
2023-09-15 18:39:32 +0900

    add:Terasurware導入。confression_master.xlsx初版の作成。

---
+++ CNF/CNF/Assets/Terasurware/Classes/Entity_confression_master.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Entity_confression_master : ScriptableObject
+{	
+	public List<Param> param = new List<Param> ();
+
+	[System.SerializableAttribute]
+	public class Param
+	{
+		
+		public int id;
+		public string villager_confression_text;
+		public string sister_empathize_text;
+		public int empathize_point_type;
+		public string sister_admonish_text;
+		public int admonish_point_type;
+		public string villager_empathize_text;
+		public int villager_empathize_manga_mark;
+		public string villager_admonish_text;
+		public int villager_admonish_manga_mark;
+	}
+}
