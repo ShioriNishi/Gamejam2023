64beee8d71e0fb81d8dad89fb225c076b2cda96d
acq_s_nishi <s_nishi@acquire.co.jp>
2023-10-19 14:37:20 +0900

    update:エンディング毎にテキストを出し分ける処理を実装

--- CNF/CNF/Assets/Scripts/EndingManager.cs
+++ CNF/CNF/Assets/Scripts/EndingManager.cs
@@ -2,31 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Confression.Defines;
+
 /// <summary>
 /// 主にエンディングの表示分けを担うマネージャー。
 /// </summary>
 public class EndingManager : MonoBehaviour
 {
-    #region field
-    [SerializeField, Tooltip("メインでセットされたリザルト情報")]
-    private EndingResultSO m_endingResultSO;
+	#region field
+	[SerializeField, Tooltip("メインでセットされたリザルト情報")]
+	private EndingResultSO m_endingResultSO;
 
-    #endregion field
+	[SerializeField, Tooltip("ベストエンド表示用画像オブジェクト")]
+	private GameObject m_bestEndingImageObject;
+	[SerializeField, Tooltip("ベストエンド表示用文言オブジェクト")]
+	private GameObject m_bestEndingTextObject;
 
-    #region public
-    #endregion public
+	[SerializeField, Tooltip("バッドエンド表示用画像オブジェクト")]
+	private GameObject m_badEndingImageObject;
+	[SerializeField, Tooltip("バッドエンド表示用文言オブジェクト")]
+	private GameObject m_badEndingTextObject;
 
-    #region private
-    // Start is called before the first frame update
-    private void Start()
-    {
-        Debug.Log($"EndingType = {m_endingResultSO.endingType}");
+	[SerializeField, Tooltip("ノーマルエンド表示用画像オブジェクト")]
+	private GameObject m_normalEndingImageObject;
+	[SerializeField, Tooltip("ベストエンド表示用文言オブジェクト")]
+	private GameObject m_normalEndingTextObject;
+
+	[SerializeField, Tooltip("狂化エンド表示用画像オブジェクト")]
+	private GameObject m_chaosEndingImageObject;
+	[SerializeField, Tooltip("狂化エンド表示用文言オブジェクト")]
+	private GameObject m_chaosEndingTextObject;
+	#endregion field
+
+	#region public
+	#endregion public
+
+	#region private
+	// Start is called before the first frame update
+	private void Start()
+	{
+		Debug.Log($"EndingType = {m_endingResultSO.endingType}");
+		ViewEnding(m_endingResultSO.endingType);
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		
+	}
+
+	private void ViewEnding(EndingType endingType)
+	{
+		// 全Endingを一旦非表示にする
+		//m_bestEndingImageObject.SetActive(false);
+		m_bestEndingTextObject.SetActive(false);
+		//m_badEndingImageObject.SetActive(false);
+		m_badEndingTextObject.SetActive(false);
+		//m_normalEndingImageObject.SetActive(false);
+		m_normalEndingTextObject.SetActive(false);
+		//m_chaosEndingImageObject.SetActive(false);
+		m_chaosEndingTextObject.SetActive(false);
+
+        switch (endingType)
+        {
+            case EndingType.BestEnding:		// ベストエンド表示
+				m_bestEndingTextObject.SetActive(true);
+                break;
+
+            case EndingType.BadEnding:      // バッドエンド表示
+				m_badEndingTextObject.SetActive(true);
+                break;
+
+            case EndingType.NormalEnding:   // ノーマルエンド表示
+				m_normalEndingTextObject.SetActive(true);
+                break;
+
+            case EndingType.ChaosEnding:    // 狂化エンド表示
+				m_chaosEndingTextObject.SetActive(true);
+                break;
+
+            default:
+                break;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    #endregion private
+	#endregion private
 }
