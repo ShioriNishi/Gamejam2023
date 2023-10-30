90b051cd05e3ece58409a2df6cb7b8f6ad0705dc
acq_s_nishi <s_nishi@acquire.co.jp>
2023-10-30 12:44:42 +0900

    refactor:懺悔マスタインポーターをタブ送りのソースに修正

--- CNF/CNF/Assets/Terasurware/Classes/Editor/confression_master_importer.cs
+++ CNF/CNF/Assets/Terasurware/Classes/Editor/confression_master_importer.cs
@@ -9,18 +9,18 @@ using NPOI.SS.UserModel;
 
 public class confression_master_importer : AssetPostprocessor
 {
-    private static readonly string filePath = "Assets/Terasurware/Excel/confression_master.xlsx";
-    private static readonly string[] sheetNames = { "confression_master", };
-    
-    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
-    {
-        foreach (string asset in importedAssets)
-        {
-            if (!filePath.Equals(asset))
-                continue;
+	private static readonly string filePath = "Assets/Terasurware/Excel/confression_master.xlsx";
+	private static readonly string[] sheetNames = { "confression_master", };
+	
+	static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+	{
+		foreach (string asset in importedAssets)
+		{
+			if (!filePath.Equals(asset))
+				continue;
 
-            using (FileStream stream = File.Open (filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
+			using (FileStream stream = File.Open (filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			{
 				IWorkbook book = null;
 				if (Path.GetExtension (filePath) == ".xls") {
 					book = new HSSFWorkbook(stream);
@@ -28,35 +28,35 @@ public class confression_master_importer : AssetPostprocessor
 					book = new XSSFWorkbook(stream);
 				}
 
-                foreach (string sheetName in sheetNames)
-                {
-                    var exportPath = "Assets/Databases/" + sheetName + ".asset";
-                    
-                    // check scriptable object
-                    var data = (Entity_confression_master)AssetDatabase.LoadAssetAtPath(exportPath, typeof(Entity_confression_master));
-                    if (data == null)
-                    {
-                        data = ScriptableObject.CreateInstance<Entity_confression_master>();
-                        AssetDatabase.CreateAsset((ScriptableObject)data, exportPath);
-                        data.hideFlags = HideFlags.NotEditable;
-                    }
-                    data.param.Clear();
+				foreach (string sheetName in sheetNames)
+				{
+					var exportPath = "Assets/Databases/" + sheetName + ".asset";
+					
+					// check scriptable object
+					var data = (Entity_confression_master)AssetDatabase.LoadAssetAtPath(exportPath, typeof(Entity_confression_master));
+					if (data == null)
+					{
+						data = ScriptableObject.CreateInstance<Entity_confression_master>();
+						AssetDatabase.CreateAsset((ScriptableObject)data, exportPath);
+						data.hideFlags = HideFlags.NotEditable;
+					}
+					data.param.Clear();
 
 					// check sheet
-                    var sheet = book.GetSheet(sheetName);
-                    if (sheet == null)
-                    {
-                        Debug.LogError("[QuestData] sheet not found:" + sheetName);
-                        continue;
-                    }
+					var sheet = book.GetSheet(sheetName);
+					if (sheet == null)
+					{
+						Debug.LogError("[QuestData] sheet not found:" + sheetName);
+						continue;
+					}
 
-                	// add infomation
-                    for (int i=1; i<= sheet.LastRowNum; i++)
-                    {
-                        IRow row = sheet.GetRow(i);
-                        ICell cell = null;
-                        
-                        var p = new Entity_confression_master.Param();
+					// add infomation
+					for (int i=1; i<= sheet.LastRowNum; i++)
+					{
+						IRow row = sheet.GetRow(i);
+						ICell cell = null;
+						
+						var p = new Entity_confression_master.Param();
 			
 					cell = row.GetCell(0); p.id = (int)(cell == null ? 0 : cell.NumericCellValue);
 					cell = row.GetCell(1); p.villager_confression_text = (cell == null ? "" : cell.StringCellValue);
@@ -71,15 +71,15 @@ public class confression_master_importer : AssetPostprocessor
 					cell = row.GetCell(10); p.villager_admonish_text = (cell == null ? "" : cell.StringCellValue);
 					cell = row.GetCell(11); p.villager_admonish_manga_mark = (int)(cell == null ? 0 : cell.NumericCellValue);
 
-                        data.param.Add(p);
-                    }
-                    
-                    // save scriptable object
-                    ScriptableObject obj = AssetDatabase.LoadAssetAtPath(exportPath, typeof(ScriptableObject)) as ScriptableObject;
-                    EditorUtility.SetDirty(obj);
-                }
-            }
+						data.param.Add(p);
+					}
+					
+					// save scriptable object
+					ScriptableObject obj = AssetDatabase.LoadAssetAtPath(exportPath, typeof(ScriptableObject)) as ScriptableObject;
+					EditorUtility.SetDirty(obj);
+				}
+			}
 
-        }
-    }
+		}
+	}
 }
